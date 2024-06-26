490d22ca3a69f3b32b6e53f3298c555cd7265e5a
Ahmed Mahfouz <ahmelsamahy@gmail.com>
2024-06-26 16:38:58 +0200

    feat<export> add function to create blank model

--- following_utils/export.py
+++ following_utils/export.py
@@ -1,60 +1,49 @@
-# This script takes a .pt model as input then exports it to .engine.
-# It needs to run on the nano or else will throw an error ("TensorRT model exported with a different version than 8.2.0.6")
-
+# # This script takes a .yaml model configuration file as input, creates a model,
+# # saves it as a .pt file, and then exports it to a .engine file.
+# # It needs to run on the nano or else will throw an error
+# # ("TensorRT model exported with a different version than 8.2.0.6")
 import os
 import shutil
 import time
-import warnings
+from datetime import datetime
 
-import tensorrt as trt
-import torch
-import ultralytics
 from ultralytics import YOLO
 
-ultralytics.checks()
+# Get today's date in the required format
+today_date = datetime.now().strftime("[%Y-%m-%dT%H:%M]")
 
-# Suppress specific warnings
-warnings.filterwarnings("ignore", category=UserWarning)
-
+# Define the base directory
+base_dir = f"/home/ftpuser/{today_date}"
+os.makedirs(base_dir, exist_ok=True)
 
 class ModelTester:
-    def __init__(self, model_name):
+    def __init__(self, model_name, img_size=(640, 480)):
+        self.img_size = img_size
         self.model_name = model_name
-        self.pt_model_path = f"{model_name}.pt"
-        self.engine_model_path = f"/home/ftpuser/{model_name}.engine"
+        self.engine_model_path = os.path.join(base_dir, f"{model_name}.engine")
+        self.best_model_path = os.path.join(base_dir, "best.pt")
 
-    def check_tensorrt():
-        try:
-            TRT_VERSION = trt.__version__
-            print(f"TensorRT version: {TRT_VERSION}")
-            return True
-        except ImportError:
-            print("TensorRT is not installed.")
-            return False
+    def create_model(self):
+        self.pt_model = YOLO(f"./{self.model_name}.yaml")  # build a new model from the .yaml configuration
+        self.pt_model.train(data="coco8.yaml", epochs=1, imgsz=(self.img_size[1], self.img_size[0]), classes=[0], device="cuda")
+        self.pt_model.val()  # evaluate model performance on the validation set
+        self.pt_model("https://ultralytics.com/images/bus.jpg")  # predict on an image
 
-    def check_cuda():
-        if torch.cuda.is_available():
-            print(f"CUDA is available. CUDA version: {torch.version.cuda}")
-        else:
-            print("CUDA is not available.")
+        # Copy the best.pt model to the base directory
+        shutil.copy("runs/detect/train/weights/best.pt", self.best_model_path)
+        print(f"Copied best.pt to {self.best_model_path}")
 
     def export_model(self):
         if not os.path.exists(self.engine_model_path):
-            pt_model = YOLO(self.pt_model_path)
-            # (you might want to change imgsz parameter here)
-            pt_model.export(format="engine", device="cuda", int8=True, imgsz=640)
-
-            default_engine_path = f"{self.model_name}.engine"
-            if os.path.exists(default_engine_path):
-                shutil.move(default_engine_path, self.engine_model_path)
-
+            # Export the model to TensorRT format with INT8 quantization
+            engine_path = self.pt_model.export(format="engine", device="cuda", imgsz=(self.img_size[1], self.img_size[0]))
+            shutil.move(engine_path, self.engine_model_path)
+            print(f"Moved the file to {self.engine_model_path}🤝🔥🔥")
 
 if __name__ == "__main__":
-    model_name = "yolov10n"
-    tester = ModelTester(model_name)
+    tester = ModelTester("yolov8n")
 
-    if tester.check_tensorrt():
-        tester.check_cuda()
-        tester.export_model()
+    tester.create_model()
+    tester.export_model()
 
     time.sleep(60000)  # This number is arbitrary and just to keep the console loading for quite some time
